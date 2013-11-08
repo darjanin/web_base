84bbad47366eb79f25ed9e707cbf4b9e1a0a695b
Milan Darjanin <me@milandarjanin.com>
2013-11-08 20:56:03 +0100

    Modified sass-convert script
    Added color outputs of the active workspace, style and paths to folders.
    After start of the script it will force to update the scss folder files.
    Change in order of the code. At the beginning is the configuration section.

--- sass-convert.sh
+++ sass-convert.sh
@@ -1,26 +1,43 @@
 #!/usr/bin/env zsh
-########################## DO NOT CHANGE THIS PART #############################
-control_c() { echo "\nBye"; exit $?; }
-trap control_c SIGINT
-################################################################################
-
+############################### CONFIGURATION ##################################
 pathToScss='./scss/'
 pathToCss='./css/'
 # workspace specify if the code is in dev (development) or dep (deployment)
 # default is dev
 workspace='dev'
 # style of the output code; nested, compact, compressed, expanded
+# if the style value is something else than empty string than it will be used
+# it will affect the settings of the style from the workspace
+style=''
+################################################################################
+
+########################## DO NOT CHANGE THIS PART #############################
+
+control_c() { echo "\n`purple_text Bye`"; exit $?; }
+trap control_c SIGINT
+
+red_text()      { echo "\e[31m$1\e[0m"; }
+green_text()    { echo "\e[32m$1\e[0m"; }
+blue_text()     { echo "\e[34m$1\e[0m"; }
+purple_text()   { echo "\e[35m$1\e[0m"; }
+
 # dev default is expanded
 # dep defualt is compressed
 if [ "$workspace" = "dev" ]; then
-    style='expanded'
-    debug='-l'
+    if [ "$style" = '' ]; then  style='expanded';  fi
+    outputMessage=`purple_text development`
 elif [ "$workspace" = "dep" ]; then
-    style='compressed'
-    debug=''
+    if [ "$style" = '' ]; then  style='compressed';  fi
+    outputMessage=`purple_text deployment`
 fi
-# uncomment following line and set your own style
-# style='your_own'
+outputMessage+=" `green_text $style`"
 
+echo $outputMessage
+echo `blue_text "scss path $pathToScss"`
+echo `blue_text " css path $pathToCss"`
+# if there was changes in the settings than it will update the css file
+sass -E "utf-8" -l --force --style "$style" --update "$pathToScss:$pathToCss"
+# start watch for changes
+sass -E "utf-8" -l --style "$style" --watch "$pathToScss:$pathToCss" --trace
 
-sass -E "utf-8" "$debug" --style "$style" --watch "$pathToScss:$pathToCss" 
+################################################################################
